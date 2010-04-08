125c5615167e98e0ba1bb5cb5d011fe5a330b4f9
Carlos Castilla <carloscmillan@gmail.com>
2010-04-08 15:13:00 +0200

    Added numbers to validkeylist and fixed any little bug in textComplete activity

--- src/clicActivitiesHandler.py
+++ src/clicActivitiesHandler.py
@@ -195,7 +195,8 @@ class ClicActivities:
         validKeyList = {'delete':'delete','backspace':'backspace','space':' ','return':'return',
                         'a':'A','b':'B','c':'C','d':'D','e':'E','f':'F','g':'G','h':'H','i':'I',
                         'j':'J','k':'K','l':'L','m':'M','n':'N','o':'O','p':'P','q':'Q','r':'R',
-                        's':'S','t':'T','u':'U','v':'V','w':'W','x':'X','y':'Y','z':'Z'}
+                        's':'S','t':'T','u':'U','v':'V','w':'W','x':'X','y':'Y','z':'Z',
+                        '1':'1','2':'2','3':'3','4':'4','5':'5','6':'6','7':'7','8':'8','9':'9','0':'0'}
         
         if key in validKeyList:
             return validKeyList[key]
